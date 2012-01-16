2f04cb9684062ed1a1376258e737a6d76e5b209f
Oleg G <verdrehung@gmail.com>
2012-01-17 00:08:51 +0700

    more options added

--- lib/LWP/UserAgent/Proxified.pm
+++ lib/LWP/UserAgent/Proxified.pm
@@ -2,27 +2,61 @@ package LWP::UserAgent::Proxified;
 
 use strict;
 use base 'LWP::UserAgent';
+use List::Util;
 
 our $VERSION = '0.00';
 
 sub new {
 	my ($class, %opts) = @_;
 	
-	my $proxylist = delete $opts{proxylist};
+	my $proxylist    = delete $opts{proxylist};
+	my $proxyrand    = delete $opts{proxyrand};
+	my $proxyshuffle = delete $opts{proxyshuffle};
+	my $proxyset_cb  = delete $opts{proxyset_cb};
 	my $self = $class->SUPER::new(%opts);
 	
-	$self->{proxylist} = $proxylist;
+	$self->{proxylist}    = $proxylist;
+	$self->{proxyrand}    = $proxyrand;
+	$self->{proxyshuffle} = $proxyshuffle;
+	$self->{proxyset_cb}  = $proxyset_cb;
+	$self->{current_proxy} = 0;
+	
 	return $self;
 }
 
 sub simple_request {
 	my $self = shift;
 	
+	my ($proxy_scheme, $proxy);
 	if (@{$self->{proxylist}}) {
-		my $i = int rand @{$self->{proxylist}};
-		$i-- unless $i%2 == 0;
+		if ($self->{proxyrand}) {
+			my $i = int rand @{$self->{proxylist}};
+			$i-- unless $i % 2 == 0;
+			
+			$proxy_scheme = $self->{proxylist}[$i];
+			$proxy        = $self->{proxylist}[$i+1];
+		}
+		else {
+			if ($self->{current_proxy} >= @{$self->{proxylist}}) {
+				$self->{current_proxy} = 0;
+				if ($self->{proxyshuffle}) {
+					@{$self->{proxylist}} = List::Util::shuffle @{$self->{proxylist}};
+				}
+			}
+			
+			$proxy_scheme = $self->{proxylist}[$self->{current_proxy}];
+			$proxy        = $self->{proxylist}[$self->{current_proxy}+1];
+			$self->{current_proxy} += 2;
+		}
 		
-		$self->proxy($self->{proxylist}[$i], $self->{proxylist}[$i+1]);
+		if (defined $self->{proxyset_cb} and ref $self->{proxyset_cb} eq 'CODE') {
+			my @rv = $self->{proxyset_cb}->($_[0], $self->{proxylist}, $proxy_scheme, $proxy);
+			if (@rv == 2) {
+				($proxy_scheme, $proxy) = @rv;
+			}
+		}
+		
+		$self->proxy($proxy_scheme, $proxy);
 	}
 	
 	return $self->SUPER::simple_request(@_);
